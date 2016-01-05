53c2139a505e6b1343693204a60173e8d2c2c717
Rafael Kueng <rafik@physik.uzh.ch>
2016-01-05 11:56:02 +0100

    clean up

--- index.php
+++ index.php
@@ -2,13 +2,18 @@
     include 'funcs.php';
     
     header('Content-Type: text/html; charset=utf-8');
-    $PAGE = $_GET['page'];
+    if (array_key_exists('page', $_GET)) {
+        $PAGE = $_GET['page'];
+    }
+    else {
+        $PAGE = NULL;
+    }
 
     // Prevent skript skiddies from fucking around
     $PAGE = basename($PAGE);
     #$PAGE = ereg_replace("\.", "GO_AWAY", $PAGE);
     #$PAGE = ereg_replace("\.\.", "GO_AWAY", $PAGE);
-    $PAGE = ereg_replace("~", "GO_AWAY", $PAGE);
+    #$PAGE = preg_replace("~", "GO_AWAY", $PAGE);
 
     if(empty($PAGE)) $PAGE = "overview";
     
@@ -47,35 +52,10 @@
   </ul>
 </nav>
 
-<article>
-  <h1>Welcome</h1>
-  <p>
-    Dear eLISA Consortium members,<br>
-    the year 2013 has brought a big step forward with the choice of «The Gravitational Universe» as an approved Science Theme with a firm launch slot as L3 mission in the ESA program.
-    The next step after the successful LISA Pathfinder flight in 2015 will be the selection of the specific mission concept to be flown.
-    To make rapid progress in this direction it has been decided by the Consortium Board to held on 12 and 13 March a full eLISA Consortium Meeting which will be held in Zurich.
-    On 10 March we will also have a workshop on «Testing Alternative Theories of Gravity with LPF».
-  </p>
-  
-  <h2>Programme</h2>
-  <p>
-    On Wednesday evening there will be a conference dinner at the Dozentenfoyer at the ETH Zürich.
-    Please indicate in your registration if you like to come
-    (cost participation: approx. CHF 50). 
-  </p>
-  
-  <h2>Participants</h2>
-  <p>
-    You can find a list of all participants here.
-  </p>
-  
-  <h2>Location</h2>
-  <p>
-    The meeting will take place at the University of Zürich Irchel campus.
-    Please check the programme for more information on the associated rooms.
-    For information about how to get there, please consult the travel information page.
-  </p>
-</article>
+<?php
+  if (file_exists("$PAGE" . ".php")) { include("$PAGE" . ".php"); }
+  else { print "<article><h1>Page not found!</h1></article>"; }
+?>
 
 <aside>
   <h1>Contact:</h1>
