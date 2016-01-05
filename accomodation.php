3b1fb9f8a23aa3b6383a000e96adcbf7dbc3a5bb
Rafael Kueng <rafik@physik.uzh.ch>
2016-01-05 20:48:31 +0100

    added accomodation

--- accomodation.php
+++ accomodation.php
@@ -1,1 +1,41 @@
- 
+<article>
+
+<h1>Hotels</h1>
+
+<p>
+  We recommend the following hotels.<br>
+  For more choice consult <a href="https://www.zuerich.com/en/visit/accommodation-hotel-bed-and-breakfast-hostel"> the webpage of Zurich tourism</a>.
+</p>
+
+<?php
+  $hotels = csv_to_array('hotels.csv');
+  
+  foreach ($hotels as $hotel) {
+    #var_dump($hotel);
+    
+    $shorttel = str_replace(' ', '', $hotel["tel"]);
+    $img = (isset($hotel["img"]) && $hotel["img"]) ? $hotel["img"] : 'generic_hotel.jpg';
+
+    echo <<<EOL
+<div class='hotel'>
+  <a class='imga' href="img/hotels/$img">
+  <img src="img/hotels/_$img" />
+  </a>
+  <h3>{$hotel["name"]}</h3>
+
+  <div class="address">
+  {$hotel["street"]}<br>
+  {$hotel["loc"]}
+  </div>
+
+  <a class="web" href="{$hotel["url"]}">{$hotel["url"]}</a>
+  <a class="mail" href="mailto:{$hotel["email"]}">contact</a>
+  <a class="tel" href="tel:$shorttel">{$hotel["tel"]}</a>
+</div>
+EOL;
+  }
+?>
+
+
+
+</article>
