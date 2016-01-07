f752c59d203e3272976e1906a9cd7ac25db822b8
Rafael Kueng <rafik@physik.uzh.ch>
2016-01-07 11:37:19 +0100

    latest changes

--- default_aside.php
+++ default_aside.php
@@ -1,11 +1,11 @@
 <aside>
-  <h1>Head</h1>
+  <h1>Local<br>Organisation<br>Committee</h1>
+  <h2>Head</h2>
   <ul>
     <li>Philippe Jetzer</li>
     <li>Domenico Giardini</li>
   </ul>
-
-  <h1>Local<br>Organisation<br>Committee</h1>
+  <h2>Members</h2>
   <ul>
     <li>Ruxandra Bondarescu</li>
     <li>Lionel Philippoz</li>
@@ -17,8 +17,8 @@
     <li>Peter Zweifel</li>
     <li>Jan Ten Pierick</li>
   </ul>
-  <h1>Contact</h1>
-  <ul>
-    <li><a href=''>put any email here</a></li>
-  </ul>
+  <h2>Contact</h2>
+  <p>
+    <a href=''>put any email here</a>
+  </p>
 </aside>
