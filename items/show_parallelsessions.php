dbfe53323d81e8e019a69aa7f797efa83ba310db
Rafael Kueng <rafi.kueng@gmx.ch>
2016-05-01 21:51:30 +0200

    add forgotten file

---
+++ items/show_parallelsessions.php
@@ -0,0 +1,22 @@
+
+<h2>Parallel Sessions</h2>
+
+
+<p>
+    The following parallel sessions are already organized.
+    More will be announced as soon as available.
+</p>
+
+<h3>1. LISA Pathfinder results</h3>
+<ul>
+    <li>Paul Mc Namara (ESA)</li>
+    <li>Bill Weber (University of Trento)</li>
+    <li>Martin Hewitson (Max Planck Institute for Gravitational Physics)</li>
+</ul>
+
+
+<h3>2. LISA Hardware</h3>
+<ul>
+    <li>Oliver Jennrich (ESA)</li>
+    <li>Gerhard Heinzel (Max Planck Institute for Gravitational Physics, Hannover)</li>
+</ul>
