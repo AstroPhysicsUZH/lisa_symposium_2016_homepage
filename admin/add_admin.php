35c22658849a4e301bfd7aa70f98a15a51f6e297
Rafael Kueng <rafik@physik.uzh.ch>
2016-05-25 19:30:45 +0200

    admin user management

---
+++ admin/add_admin.php
@@ -0,0 +1,59 @@
+<?php
+require_once "lib/header.php";
+?>
+
+<h1>Admin user Management</h1>
+
+<h2>Create new user</h2>
+<form id="registration" class="controlbox"
+      name="new user registration" method="POST"
+      action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
+      >
+	<input type="hidden" name="op" value="register"/>
+	<input type="hidden" name="sha1" value=""/>
+	<table>
+		<tr><td>user name </td><td><input type="text" name="username" value="" /></td></tr>
+		<tr><td>email address </td><td><input type="text" name="email" value="" /></td></tr>
+		<tr><td>password </td><td><input type="password" name="password1" value="" /></td></tr>
+        <tr><td>password (again) </td><td><input type="password" name="password2" value="" /></td></tr>
+        <tr><td>role </td><td><input type="text" name="role" value="" /></td></tr>
+	</table>
+	<input type="button" value="register" onclick="User.processRegistration()"/>
+</form>
+
+<h2>Reset user passwords</h2>
+
+<table>
+
+<?php
+$dbfile = User::DATABASE_LOCATION  . User::DATABASE_NAME . ".db";
+$udb = new PDO("sqlite:" . $dbfile);
+$query = "SELECT * FROM users";
+
+
+foreach($udb->query($query) as $data) {
+    print "
+    <tr>
+        <td>{$data['username']}</td>
+        <td>{$data['email']}</td>
+        <td>{$data['role']}</td>
+        <td>
+            <form class='controlbox' name='unregister'
+                id='unregister{$data['username']}'
+                action='index.php'
+                method='POST'>
+                <input type='hidden' name='op' value='unregister'/>
+                <input type='hidden' name='username' value='{$data['username']}' />
+                <input type='submit' value='DEL' />
+            </form>
+        </td>
+    </tr>";
+}
+
+?>
+
+</table>
+
+<?php
+require_once "lib/footer.php";
+?>
