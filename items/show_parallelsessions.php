ab1d56e8ffd7464a3352563ff0fee8087b259757
Rafael Kueng <rafik@physik.uzh.ch>
2016-05-25 19:20:57 +0200

    parasess changes by PJ

--- items/show_parallelsessions.php
+++ items/show_parallelsessions.php
@@ -1,14 +1,16 @@
 
 <h2>Parallel Sessions</h2>
 
-
 <p>
-    The following parallel sessions are already organized.
+    The following parallel sessions are foreseen.
     More will be announced as soon as available.
 </p>
 
 <h3>1. LISA Pathfinder - Results</h3>
-<ul>
+
+<p class="orgatime">???</p>
+<p class="orgas">Organizers:</p>
+<ul class="orgalist">
     <li>Paul Mc Namara (ESA)</li>
     <li>Bill Weber (University of Trento)</li>
     <li>Martin Hewitson (Max Planck Institute for Gravitational Physics)</li>
@@ -16,25 +18,40 @@
 
 
 <h3>2. LISA Hardware</h3>
-<ul>
+
+<p class="orgatime">???</p>
+<p class="orgas">Organizers:</p>
+<ul class="orgalist">
     <li>Oliver Jennrich (ESA)</li>
     <li>Gerhard Heinzel (Max Planck Institute for Gravitational Physics, Hannover)</li>
 </ul>
 
+
 <h3>3. Astrophysics with Gravitational Waves</h3>
-<ul>
+
+<p class="orgatime">???</p>
+<p class="orgas">Organizers:</p>
+<ul class="orgalist">
     <li>Alberto Sesana (University of Birmingham)</li>
     <li>Monica Colpi (Università degli Studi di Milano-Bicocca)</li>
 </ul>
 
+
 <h3>4. Extreme Mass Ratio Inspirals (EMRI)</h3>
-<ul>
+
+<p class="orgatime">???</p>
+<p class="orgas">Organizers:</p>
+<ul class="orgalist">
     <li>Carlos Sopuerta (Institute of Space Sciences CSIC-IEEC, Catalunya, Spain)</li>
     <li>Pau Amaro-Seoane (Albert Einstein Institute, Max Planck Institute for Gravitational Physics, Potsdam)</li>
 </ul>
 
+
 <h3>5. Cosmology and Gravitational Waves</h3>
-<ul>
+
+<p class="orgatime">Thu, 2016-09-08</p>
+<p class="orgas">Organizers:</p>
+<ul class="orgalist">
     <li>Chiara Caprini (IPhT, CEA Saclay)</li>
     <li>Germano Nardini (Albert Einstein Center for Fundamental Physics, Universität Bern)</li>
 </ul>
