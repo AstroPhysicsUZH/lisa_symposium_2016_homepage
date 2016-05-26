85140a632af592141dbda601ec3bd9fde4e41dc6
Rafael Kueng <rafik@physik.uzh.ch>
2016-05-26 15:48:30 +0200

    hardening server

--- admin/lib/menu.php
+++ admin/lib/menu.php
@@ -6,8 +6,8 @@ $admin_modules = array(
     ["add_admin.php", "Edit Admin Users", ["admin"]],
     ["edit.php", "Edit Entries", ["admin", "helper"]],
     ["payment_np.php", "Manage Payment", ["admin", "finanz"]],
-    ["payment_all.php", "Manage Payment (all)", ["admin", "finanz"]],
-    ["test2.php", "testing", ["admin"]]
+    ["payment_all.php", "Manage Payment (all)", ["admin", "finanz"]]
+    #["test2.php", "testing", ["admin"]]
 );
 
 $get_acl = [];
