e693c74addc9b5c0259244fe0ffa72e6ee8e2921
Rafael Kueng <rafik@physik.uzh.ch>
2016-05-27 14:22:33 +0200

    modify paralelsession to make it clearer

--- items/show_parallelsessions.php
+++ items/show_parallelsessions.php
@@ -1,5 +1,5 @@
 
-<h2>Parallel Sessions</h2>
+<h2>Parallel Sessions and Organizers</h2>
 
 <p>
     The following parallel sessions are foreseen.
@@ -9,9 +9,9 @@
 <h3>1. LISA Pathfinder - Results</h3>
 
 <p class="orgatime"> </p>
-<p class="orgas">Organizers:<br />
-    Paul Mc Namara (ESA),
-    Bill Weber (University of Trento),
+<p class="orgas"><b>Organizers:</b><br />
+    Paul Mc Namara (ESA);
+    Bill Weber (University of Trento);
     Martin Hewitson (Max Planck Institute for Gravitational Physics)
 </p>
 
@@ -19,38 +19,34 @@
 <h3>2. LISA Hardware</h3>
 
 <p class="orgatime"> </p>
-<p class="orgas">Organizers:</p>
-<ul class="orgalist">
-    <li>Oliver Jennrich (ESA)</li>
-    <li>Gerhard Heinzel (Max Planck Institute for Gravitational Physics, Hannover)</li>
+<p class="orgas"><b>Organizers:</b><br />
+    Oliver Jennrich (ESA);
+    Gerhard Heinzel (Max Planck Institute for Gravitational Physics, Hannover)
 </ul>
 
 
 <h3>3. Astrophysics with Gravitational Waves</h3>
 
 <p class="orgatime"> </p>
-<p class="orgas">Organizers:</p>
-<ul class="orgalist">
-    <li>Alberto Sesana (University of Birmingham)</li>
-    <li>Monica Colpi (Università degli Studi di Milano-Bicocca)</li>
+<p class="orgas"><b>Organizers:</b><br />
+    Alberto Sesana (University of Birmingham);
+    Monica Colpi (Università degli Studi di Milano-Bicocca)
 </ul>
 
 
 <h3>4. Extreme Mass Ratio Inspirals (EMRI)</h3>
 
 <p class="orgatime"> </p>
-<p class="orgas">Organizers:</p>
-<ul class="orgalist">
-    <li>Carlos Sopuerta (Institute of Space Sciences CSIC-IEEC, Catalunya, Spain)</li>
-    <li>Pau Amaro-Seoane (Albert Einstein Institute, Max Planck Institute for Gravitational Physics, Potsdam)</li>
+<p class="orgas"><b>Organizers:</b><br />
+    Carlos Sopuerta (Institute of Space Sciences CSIC-IEEC, Catalunya, Spain);
+    Pau Amaro-Seoane (Albert Einstein Institute, Max Planck Institute for Gravitational Physics, Potsdam)
 </ul>
 
 
 <h3>5. Cosmology and Gravitational Waves</h3>
 
 <p class="orgatime">Thu, 2016-09-08</p>
-<p class="orgas">Organizers:</p>
-<ul class="orgalist">
-    <li>Chiara Caprini (IPhT, CEA Saclay)</li>
-    <li>Germano Nardini (Albert Einstein Center for Fundamental Physics, Universität Bern)</li>
+<p class="orgas"><b>Organizers:</b><br />
+    Chiara Caprini (IPhT, CEA Saclay);
+    Germano Nardini (Albert Einstein Center for Fundamental Physics, Universität Bern)
 </ul>
