78a70ff3d449f0f8732ffe8d53e9a498247570e5
Rafael Kueng <rafik@physik.uzh.ch>
2016-06-23 20:32:19 +0200

    bugfix

--- user/lib/menu.php
+++ user/lib/menu.php
@@ -6,7 +6,7 @@
       <li><a href="submission.php">Edit My Submission</a></li>
       <li><a class="deactivated" href="proceeding.php">Edit My Proceeding</a></li>
       <li><a href="invoice_.php">Get Invoice</a></li>
-      <li><a href="invitation.php">Get Invitation</a></li>
+      <li><a href="invitation.php">Get Invitation Letter</a></li>
       <li><a href="../">&lt;&mdash; back</a></li>
   </ul>
 
