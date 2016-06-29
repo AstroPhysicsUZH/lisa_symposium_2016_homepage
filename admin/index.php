907981ba3baac49a3ee87a33e3d3239d5e141a94
Rafael Kueng <rafi.kueng@gmx.ch>
2016-06-30 00:54:58 +0200

    more verbose index

--- admin/index.php
+++ admin/index.php
@@ -2,17 +2,41 @@
 
 <?php
 $nRows = (int)$db->query("SELECT count(*) FROM {$tableName}")->fetchColumn();
+$nPayed = (int)$db->query("SELECT SUM(CASE WHEN hasPayed THEN 1 ELSE 0 END) FROM  {$tableName}")->fetchColumn();
+
 $nLunch = (int)$db->query("SELECT sum(nPersons) from {$tableName}")->fetchColumn();
 $nVeggies = (int)$db->query("SELECT SUM(CASE WHEN isVeggie THEN nPersons ELSE 0 END) FROM {$tableName}")->fetchColumn();
+$nWLAN = (int)$db->query("SELECT SUM(CASE WHEN needInet THEN 1 ELSE 0 END) FROM  {$tableName}")->fetchColumn();
+$nImpared = (int)$db->query("SELECT SUM(CASE WHEN isImpaired THEN 1 ELSE 0 END) FROM  {$tableName}")->fetchColumn();
+
+$nContributions = (int)$db->query("SELECT SUM(CASE WHEN talkType THEN 1 ELSE 0 END) FROM  {$tableName}")->fetchColumn();
+$nTalks = (int)$db->query("SELECT SUM(CASE WHEN talkType=1 THEN 1 ELSE 0 END) FROM  {$tableName}")->fetchColumn();
+$nPosters = (int)$db->query("SELECT SUM(CASE WHEN talkType=2 THEN 1 ELSE 0 END) FROM  {$tableName}")->fetchColumn();
+$nCategorised = (int)$db->query("SELECT SUM(CASE WHEN presentationCategories<>'' THEN 1 ELSE 0 END) FROM  {$tableName}")->fetchColumn();
+$nSession = (int)$db->query("SELECT SUM(CASE WHEN assignedSession<>'' THEN 1 ELSE 0 END) FROM  {$tableName}")->fetchColumn();
+
+
 ?>
 
-<h1>Welcome back</h1>
-<h2>Overview</h2>
+<h1>Welcome</h1>
+<h2>We currently know, that...</h2>
 <p>
-    We currently have:<br />
-    <?=$nRows?> Persons registered <br />
-    <?=$nLunch?> Lunchs to be served (of that <?=$nVeggies?> Veggies) <br />
+    ... <?=$nRows?> Persons registered <br />
+    ... of those, <?=$nPayed?> Persons already payed the bill.<br />
+    ... But that means, <b><?=$nRows-$nPayed?> are still open to be payed!</b>
 </p>
+<p>
+    ... <?=$nLunch?> Lunchs to be served (of that <?=$nVeggies?> Veggies) <br />
+    ... <?=$nWLAN?> WLAN accounts have to be ordered. <br />
+    ... <?=$nImpared?> people need eTaxi transport to the dinner restaurant.
+</p>
+<p>
+    ... <?=$nContributions?> Contributions (<?=$nTalks?> talks; <?=$nPosters?> posters)<br />
+    ... <?=$nCategorised?> are categorized, <?=$nSession?> even already have a session assigned <br />
+    ... But that means, <b><?=$nContributions-$nSession?> are still open to be assigned!</b><br />
+
+
+
 
 
 <?php require "lib/footer.php" ?>
