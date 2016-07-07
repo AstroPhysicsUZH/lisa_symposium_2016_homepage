df905609f99bf1f65fa57dc04028993e60dacb48
Rafael Kueng <rafik@physik.uzh.ch>
2016-07-07 19:14:47 +0200

    better pleanery speaker

--- items/plenary_speakers.php
+++ items/plenary_speakers.php
@@ -1,104 +1,275 @@
+<?php
 
+$topics = [
+    "on LISA Pathfinder" => [
+        [
+            'name' => "Paul Mc Namara",
+            'affil' => "ESA",
+            'title' => "LISA Pathfinder: First steps to observing gravitational waves from space",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Stefano Vitale",
+            'affil' => "University of Trento",
+            'title' => "The LTP experiment on LISA Pathfinder and its first results",
+            'abstract' => ""
+        ],
+        [
+            'regid' => 76,
+            'name' => "Bill Weber",
+            'affil' => "University of Trento",
+            'title' => "Physical model of the LISA Pathfinder differential acceleration measurement and its application to LISA",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Martin Hewitson",
+            'affil' => "Max Planck Institute for Gravitational Physics; Hannover",
+            'title' => "In-flight Performance of the Optical Metrology System on LISA Pathfinder",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Harry Ward",
+            'affil' => "Institute for Gravitational Research, University of Glasgow",
+            'title' => "",
+            'abstract' => ""
+        ]
+    ],
+
+    "on LISA" => [
+        [
+            'regid' => 8,
+            'name' => "Karsten Danzmann",
+            'affil' => "Max Planck Institute for Gravitational Physics; Hannover",
+            'title' => "The Status of LISA",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Oliver Jennrich",
+            'affil' => "ESA",
+            'title' => "",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Frederic Safa",
+            'affil' => "ESA",
+            'title' => "",
+            'abstract' => ""
+        ]
+    ],
+
+    "ESA/NASA Plans" => [
+        [
+            'name' => "Alvaro Giménez Cañete",
+            'affil' => "Director of Science, ESA",
+            'title' => "",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Paul Hertz",
+            'affil' => "NASA, Director, Astrophysics Division, Science Mission Directorate",
+            'title' => "",
+            'abstract' => ""
+        ]
+    ],
+
+    "Grace &ndash; Follow on discussion" => [
+        [
+            'regid' => 80,
+            'name' => "Gerhard Heinzel",
+            'affil' => "Max Planck Institute for Gravitational Physics; Hannover",
+            'title' => "Satellite interferometry from LTP and GRACE Follow-On to LISA",
+            'abstract' => "This talk will summarize the state of laser interferometry in space, from the now proven local interferometry on LISA Pathfinder to the soon to be launched long-distance interferometry on GRACE Follow-On and previous developments for LISA, and list what still is missing for LISA."
+        ]
+    ],
+
+    "Microscope" => [
+        [
+            'name' => "Joel Bergé",
+            'affil' => "French Aeropace Lab ONERA; Palaiseau",
+            'title' => "",
+            'abstract' => ""
+        ]
+    ],
+
+    "LIGO / Advanced VIRGO" => [
+        [
+            'name' => "Fulvio Ricci",
+            'affil' => "Università La Sapienza; Roma",
+            'title' => "Advanced VIRGO: State of the Art",
+            'abstract' => ""
+        ],
+        [
+            'regid' => 85,
+            'name' => "Katherine Dooley",
+            'affil' => "University of Mississippi",
+            'title' => "News from the Dark Universe",
+            'abstract' => "After over 50 years of effort, scientists and engineers recently succeeded in the challenge to measure the infinitesimal changes in the separation between two mirrors caused by cataclysmic events in the distant universe. During its first observing run in fall 2015, the Advanced LIGO interferometers witnessed the passing of gravitational waves created by the collisions of black holes over 1 billion light-years away. These first and second detections of gravitational waves bring to life a new instrument for astronomy, delivering news from the otherwise invisible affairs of the cosmos. I will describe the Advanced LIGO interferometers, the detections that have been made, and the outlook of ongoing and planned instrument work to further improve Advanced LIGO's sensitivity for future observing runs."
+        ]
+    ],
+
+    "Astrophysics and Cosmology with GW" => [
+        [
+            'name' => "Pierre Binétruy",
+            'affil' => "Université Paris-Diderot",
+            'title' => "",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Roger Blandford",
+            'affil' => "Stanford University",
+            'title' => "The Future of Black Hole Astrophysics in the LISA-VIRGO-LPF Era",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Kelly Holley-Bockelmann",
+            'affil' => "Vanderbilt University; Nashville, Tennessee",
+            'title' => "",
+            'abstract' => ""
+        ]
+    ],
+
+    "Pulsar Timing Array" => [
+        [
+            'name' => "Chiara Mingarelli",
+            'affil' => "California Institute of Technology and Max Planck Institute for Radio Astronomy",
+            'title' => "The Gravitational-Wave Universe Seen by Pulsar Timing Arrays",
+            'abstract' => ""
+        ]
+    ],
+
+    "Einstein Telescope" => [
+        [
+            'name' => "B. S. Sathyaprakash",
+            'affil' => "Cardiff University",
+            'title' => "",
+            'abstract' => ""
+        ]
+    ],
+
+    "Science with LIGO/LISA" => [
+        [
+            'regid' => 92,
+            'name' => "Antoine Klein",
+            'affil' => "Université Paris-Diderot",
+            'title' => "",
+            'abstract' => ""
+        ],
+        [
+            'name' => "Antoine Petiteau",
+            'affil' => "Université Paris-Diderot",
+            'title' => "eLISA science performances in the context of LISAPathfinder first results",
+            'abstract' => ""
+        ],
+        [
+            'regid' => 98,
+            'name' => "Alberto Sesana",
+            'affil' => "University of Birmingham",
+            'title' => "The promise of multi-band gravitational wave astronomy",
+            'abstract' => "I will discuss the implications of the recent LIGO discoveries for LISA. BHBs such GW150914 will be observable by LISA years before coalescence, making possible to see the same sources at different times in different GW bands. This has a number of interesting consequences and has the potential of enhance the scientific payouts of GW detections enormously."
+        ],
+        [
+            'name' => "Bernard Schutz",
+            'affil' => "Max Planck Institute for Gravitational Physics, Potsdam",
+            'title' => "LIGO's Black Holes and Science in the LISA Era",
+            'abstract' => ""
+        ],
+    ],
+
+    "KAGRA and DECIGO" => [
+        [
+            'name' => "Shuichi Sato",
+            'affil' => "Hosei University, Tokyo",
+            'title' => "",
+            'abstract' => ""
+        ]
+    ],
+
+    "EM follow up" => [
+        [
+            'name' => "Stephen Smartt",
+            'affil' => "Queen's University Belfast",
+            'title' => "",
+            'abstract' => ""
+        ]
+    ],
+];
+/* template
+"on LISA Pathfinder" => [
+    [
+        'name' => "",
+        'affil' => "",
+        'title' => "",
+        'abstract' => ""
+    ]
+*/
+?>
 
 <h2>Preliminary List of Plenary Speakers</h2>
+<p>
+    Click on the titles to see abstracts (if available)
+</p>
 
-<h4>on LISA Pathfinder</h4>
-<ul>
-    <li>
-        Paul Mc Namara &mdash; LISA Pathfinder: First steps to observing gravitational waves from space <br />
-        (ESA)
+<?php
+
+$i = 0;
+$j = 0;
+
+foreach($topics as $topic => $speakers) {
+    echo "<h3>$topic</h3>\n<ul class='speakers'>\n";
+    foreach ($speakers as $speaker) {
+        $id = "" . $i . "_" . $j;
+?>
+    <li class="speaker_topic">
+        <span class="speaker"><?=$speaker['name']?></span>
+        <span class="affiliation"><?=$speaker['affil']?></span>
+<?php
+        if (! empty($speaker['title'])) {
+            $has_abstract = !empty($speaker['abstract']);
+            if (!$has_abstract) {
+?>
+        <a class="title">
+            &lsaquo; <?=$speaker['title']?> &rsaquo;
+        </a>
+<?php
+            }
+            else {
+ ?>
+        <a class="title linked" onclick="
+            document.getElementById('mod_abstr_<?=$id?>').style.display = 'block';
+            ">
+            &lsaquo; <?=$speaker['title']?> &rsaquo; [&hellip;]
+        </a>
+        <div id="mod_abstr_<?=$id?>" class="modal_abstract" onclick="
+            document.getElementById('mod_abstr_<?=$id?>').style.display = 'none';
+            ">
+            <div class="container">
+                <a class="close" onclick="
+                    document.getElementById('mod_abstr_<?=$id?>').style.display = 'none';
+                    ">
+                    [ close ]
+                </a>
+                <p onclick="
+                    var event = arguments[0] || window.event;
+                    if (event.stopPropagation) { event.stopPropagation(); }
+                    else { event.cancelBubble = true; }
+                    ">
+                    <span class="mod_name"><?=$speaker['name']?></span>
+                    <span class="mod_aff"><?=$speaker['affil']?></span>
+                    <span class="mod_title"><?=$speaker['title']?></span>
+                    <?=$speaker['abstract']?>
+                </p>
+            </div>
+        </div>
+<?php
+            }
+        }
+?>
     </li>
-    <li>
-        Stefano Vitale &mdash; The LTP experiment on LISA Pathfinder and its first results<br />
-        (University of Trento)</li>
-    <li>
-        Bill Weber &mdash; Physical model of the LISA Pathfinder differential acceleration measurement and its application to LISA<br />
-        (University of Trento)
-    </li>
-    <li>
-        Martin Hewitson &mdash; In-flight Performance of the Optical Metrology System on LISA Pathfinder<br>
-        (Max Planck Institute for Gravitational Physics; Hannover)
-    </li>
-    <li>Harry Ward (Institute for Gravitational Research, University of Glasgow)</li>
-</ul>
-
-<h4>on LISA</h4>
-<ul>
-    <li>
-        Karsten Danzmann &mdash; The Status of LISA<br />
-        (Max Planck Institute for Gravitational Physics; Hannover)
-    </li>
-    <li>Oliver Jennrich (ESA)</li>
-</ul>
-
-<h4>ESA/NASA Plans</h4>
-<ul>
-    <li>Alvaro Giménez Cañete (Director of Science, ESA)</li>
-    <li>Paul Hertz (NASA, Director, Astrophysics Division, Science Mission Directorate)</li>
-</ul>
-
-<h4>Grace &ndash; Follow on discussion</h4>
-<ul>
-    <li>
-        Gerhard Heinzel &mdash; Satellite interferometry from LTP and GRACE Follow-On to LISA<br />
-        (Max Planck Institute for Gravitational Physics; Hannover)
-    </li>
-</ul>
-
-<h4>Microscope</h4>
-<ul>
-    <li>Joel Bergé (French Aeropace Lab ONERA; Palaiseau)</li>
-</ul>
-
-<h4>LIGO / Advanced VIRGO</h4>
-<ul>
-    <li>
-        Fulvio Ricci &mdash; Advanced VIRGO: State of the Art <br />
-        (Università La Sapienza; Roma)
-    </li>
-    <li>Katherine Dooley (University of Mississippi)</li>
-</ul>
-
-<h4>Astrophysics and Cosmology with GW</h4>
-<ul>
-    <li>Pierre Binétruy (Université Paris-Diderot)</li>
-    <li>
-        Roger Blandford &mdash; The Future of Black Hole Astrophysics in the LISA-VIRGO-LPF Era<br />
-        (Stanford University)
-    </li>
-    <li>Kelly Holley-Bockelmann (Vanderbilt University; Nashville, Tennessee)</li>
-</ul>
-
-<h4>Pulsar Timing Array</h4>
-<ul>
-    <li>
-        Chiara Mingarelli &mdash; The Gravitational-Wave Universe Seen by Pulsar Timing Arrays <br>
-        (California Institute of Technology and Max Planck Institute for Radio Astronomy)
-    </li>
-</ul>
-
-<h4>Einstein Telescope</h4>
-<ul>
-    <li>B. S. Sathyaprakash (Cardiff University)</li>
-</ul>
-
-<h4>Science with LIGO/LISA</h4>
-<ul>
-    <li>Antoine Klein (Université Paris-Diderot)</li>
-    <li>
-        Antoine Petiteau &mdash; eLISA science performances in the context of LISAPathfinder first results<br>
-        (Université Paris-Diderot)
-    </li>
-    <li>Alberto Sesana (University of Birmingham)</li>
-    <li>Bernard Schutz (Max Planck Institute for Gravitational Physics, Potsdam)</li>
-</ul>
-
-<h4>KAGRA and DECIGO</h4>
-<ul>
-    <li>Shuichi Sato (Hosei University, Tokyo)</li>
-</ul>
-
-<h4>EM follow up</h4>
-<ul>
-    <li>Stephen Smartt (Queen's University Belfast) </li>
-</ul>
+<?php
+        $j+=1;
+    }
+    echo "</ul>";
+    $i+= 1;
+    $j = 0;
+}
+?>
