b58e8d26f11aa568810eb71d43d9bcef98aa2bb2
Rafael Kueng <rafik@physik.uzh.ch>
2016-08-12 11:03:02 +0200

    fix csv generation

--- admin/get_csv.php
+++ admin/get_csv.php
@@ -4,10 +4,26 @@ require "lib/headerphp.php";
 
 $all_people = $db->query( "SELECT * FROM {$tableName}")->fetchAll(PDO::FETCH_OBJ);
 
+$values = [
+    'id',
+    'title',
+    'firstname',
+    'lastname',
+    'email',
+    'affiliation',
+    'address',
+    'isPassive',
+    'hasPayed',
+    'amountPayed',
+
+];
+
+
 foreach($all_people as $p) {
 
-    foreach ($p as $key => $value) {
-        print json_encode($value) . '; ';
+    foreach ($values as $key) {
+
+        print json_encode($p->$key) . ';';
     }
     print "\n";
 }
