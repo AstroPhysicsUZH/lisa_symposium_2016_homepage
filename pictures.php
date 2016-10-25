5bba8c22428028fa1aadcaf5fc2ac47240836caf
Rafael Kueng <rafik@physik.uzh.ch>
2016-10-25 15:05:56 +0200

    order photos by date

--- pictures.php
+++ pictures.php
@@ -1,15 +1,32 @@
 <?php
 
 
-$directory = "photos/";
+$basedir = "photos";
 
-$images1 = glob("" . $directory . "*.jpg");
-$images2 = glob("" . $directory . "*.JPG");
-$imgs = '';
-foreach($images1 as $image){ $imgs[] = "$image"; }
-foreach($images2 as $image){ $imgs[] = "$image"; }
+$cats = [];
+$imgs = [];
 
-# print_r(basename($images2[0]));
+$subpaths = glob($basedir . '/*' , GLOB_ONLYDIR);
+natsort($subpaths);
+
+foreach($subpaths as $subpath) {
+
+    $dir = explode('/', $subpath);
+    $subdir = $dir[1];
+
+    #print_r($subdir);
+
+    if ($subdir=="_thumbnails"){continue;}
+
+    $cats[] = $subdir;
+
+    $images = glob($subpath . "/*.[jJ][pP][gG]");
+    # $imgs = '';
+    foreach($images as $image){
+        $imgs[] = ["$image", basename($image), $subpath, $subdir];
+    }
+}
+# print_r($imgs);
 ?>
 
 
@@ -19,15 +36,26 @@ foreach($images2 as $image){ $imgs[] = "$image"; }
 <h1>Pictures</h1>
 
 
+<ul>
 <?php
 
-foreach ($imgs as $img) {
-  echo "
-  <p>
-    <a href='{$img}'>
-      <img src='{$directory}thumbnail/" . basename($img) ."' height='200' alt='conference image'/>
-    </a>
-  </p>";
+foreach ($cats as $cat) {
+    print "<li><a href='#$cat'>$cat</a></li>";
+
+}
+print "</ul>";
+
+foreach ($cats as $cat) {
+    print "<h2 id='$cat'>$cat</h2>";
+
+    foreach ($imgs as $img_d) {
+      echo "
+      <p>
+        <a href='{$img_d[0]}'>
+          <img src='{$basedir}/_thumbnails/" . $img_d[1] ."' height='200' alt='conference image'/>
+        </a>
+      </p>";
+    }
 }
 
  ?>
