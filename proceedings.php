a9525204e348cfd59cbc752732d76ebd70fca657
Rafael Kueng <rafik@physik.uzh.ch>
2016-10-25 11:34:24 +0200

    modified proceedings

--- proceedings.php
+++ proceedings.php
@@ -3,55 +3,20 @@
 
 <h1>LISA11 proceeding submission site</h1>
 <p>
-    Welcome to the paper submission site for the proceedings of the 11th International LISA Symposium. The proceedings will appear in the open access Journal of Physics: Conference Series. All papers published in JPCS are fully citable and upon publication will be free to download in perpetuity.
+    The proceedings will appear in the open access Journal of Physics: Conference Series.
+    Please follow <a href="https://lisa11.conferenceseries.iop.org/">the link to the submission site</a>, create an account and upload your proceeding.
 </p>
-<p class="waring">
-    The submission deadline is: 30 November 2016.
+<a href="https://lisa11.conferenceseries.iop.org/">
+    <p class="notice" style="text-align:center;">
+    <b>submission site</b>
 </p>
+</a>
 <p>
-    Authors should register themselves by clicking 'Register' on the menu above. Once registered and logged in, click 'My Account' and select 'Submit Full Paper'.
+    Please note that:
 </p>
-<p>
-    <b>Important: please read before you submit a paper</b>
-    <br>
-    Conference Series operates a publishing licence, under which authors retain copyright of their papers and they no longer need to sign and submit copyright assignment forms. Any author who wishes to publish in IOP Conference Series must agree to the terms of the licence and by submitting a paper for publication it is assumed all authors of the paper agree, in full, to the terms of the licence. The licence terms and conditions are available online at http://conferenceseries.iop.org/content/quick_links/IOP Proceedings Licence.
+<p class="warning">
+    The submission deadline is 30 November 2016.
 </p>
 
-<h2>Preparing your paper</h2>
-
-<p>
-    IOP Conference Series uses author-supplied PDFs for all online and print publication. Authors are asked to prepare their papers using Microsoft Word or LaTeX, according to the journal guidelines and templates, and then convert these files to PDF. Full instructions on preparing your paper, along with templates, can be found on the IOP Conference Series website at: http://conferenceseries.iop.org/content/authors.
-</p>
-<p>
-    It is important to ensure that when you submit your paper, it is in its final form ready for publication, and has been thoroughly proofread. IOP do not copyedit papers and will not send out author proofs prior to publication. Please note: post-publication changes are not usually permitted, unless there are exceptional circumstances, so please ensure your paper has been checked for errors.
-</p>
-
-<h2>What you need to provide:</h2>
-<p>
-    The PDF file of your paper: when preparing your paper ensure that you follow the author guidelines. When preparing your paper please check that:
-</p>
-<ul>
-    <li>Paper size is European A4.
-    <li>Margins are at least 25mm all round.
-    <li>The paper includes the author name and affiliation (full address including country).
-    <li>There are no page numbers, or headers and footers, within the paper.
-    <li>The PDF is free of formatting errors (e.g. corrupt equations, missing or poor resolution figures), since conversion from Word to PDF can introduce formatting errors.
-    <li>Text is single spaced, not double spaced.
-    <li>The PDF file is editable and not password protected.
-    <li>All pages are portrait (landscape pages should be rotated).
-    <li>Reference lists are checked for accuracy. References can only be linked via CrossRef if they are correct and complete.
-    <li>Figures are placed within the text, not collected at the end of the document.
-    <li>A thorough proof read is conducted to check standard of English and ensure wording is clear and concise.
-    <li>Ensure that your paper includes an abstract.
-    <li>Any multimedia files you wish to publish online
-    <li>JPCS encourages authors to submit multimedia files to enhance the online versions of published research articles.
-</ul>
-<p>
-    Journal enquiries
-    General enquiries regarding IOP Conference Series should be directed to:
-    Sarah Toms
-    IOP Conference Series
-    E-mail: conferenceseries@iop.org
-</p>
 
 </article>
